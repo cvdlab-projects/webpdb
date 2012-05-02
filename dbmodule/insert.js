2570a4728b0a5913fb6567565602410da801580b
Dario Pasquantonio <dario@MacBook-Pro-di-Dario-Pasquantonio.local>
2012-05-02 16:15:24 +0200

    insert with user and password

--- dbmodule/insert.js
+++ dbmodule/insert.js
@@ -1,8 +1,13 @@
 var cradle = require('cradle');
 
-var insert = function(host, port, dbName, id, json){
+var insert = function(id, json, userName, password, dbName, host, port){
 
-var c = new(cradle.Connection)(host, port);
+var host = host || "127.0.0.1";
+var port = port || 5984;
+
+var c = new(cradle.Connection)(host, port, {
+	auth: {username: userName, password: password}
+	});
 
   var db = c.database(dbName);
 
