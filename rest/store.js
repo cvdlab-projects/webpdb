cb77dcb0e76c03d0d8880ab9139076d15e0aab0a
Marco Ungania <mar.ungania@gmail.com>
2012-05-07 18:22:39 +0200

    tolto salvataggio su file

--- rest/store.js
+++ rest/store.js
@@ -2,6 +2,7 @@ var fs = require('fs'); //Fylesystem Module, needed to store the Json file.
 
 var storeJson = exports.storeJson = function(bool, Data){
 	if(bool){
+		/*
 		fs.writeFile('Protein.json', Data , function(err) {
     		if(err) {
         	console.log(err);
@@ -9,7 +10,7 @@ var storeJson = exports.storeJson = function(bool, Data){
     		else {
         	console.log("The file was saved!");
   			}
-		});
+		});*/
 	}	
 	else{console.log(Data);}
 }
