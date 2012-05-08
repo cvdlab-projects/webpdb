a5230c864229bbd827b97616d625b3974d699b13
Dario Pasquantonio <dario@MacBook-Pro-di-Dario-Pasquantonio.local>
2012-05-08 16:20:42 +0200

    modifiche

--- dbmodule/get.js
+++ dbmodule/get.js
@@ -48,5 +48,65 @@ var retrieveByName = function(name, callbackFunction, userName, password, dbName
 	});
 };
 
+var retrieveByName = function(name, callbackFunction, userName, password, dbName, host, port){
+	var host = host || "127.0.0.1";
+	var port = port || 5984;
+
+	var c = new(cradle.Connection)(host, port, {
+		auth: {username: userName, password: password}
+	});
+
+	var db = c.database(dbName);
+
+	db.save('_design/proteinsByName', {
+		byName: {
+			map: function (doc) { if (doc.name) { emit(doc.name, doc); } }}
+		});
+		
+	db.view('proteinsByName/byName',  function (err, doc) {
+		if ( err !== null ) {
+			callbackFunction(false, err);
+		} else {
+			delete doc._rev;
+			if(doc.name !== undefined && doc.name.indexOf(0)===('f')){callbackFunction(true, doc);}
+			
+		}
+	});
+};
+
+
+
+
+var genQuery = function(string, callbackFunction){
+	var host = host || "127.0.0.1";
+	var port = port || 5984;
+	var dbName = 'starwars'; 
+	var userName = '';
+	var password = '';
+	var c = new(cradle.Connection)(host, port, {
+		auth: {username: userName, password: password}
+	});
+
+	var db = c.database(dbName);
+
+	db.save('_design/proteinsByBe', {
+		byBe: {
+			map: string}
+		});
+
+	db.view('proteinsByBe/byBe',  function (err, doc) {
+		if ( err !== null ) {
+			callbackFunction(false, err);
+		} else {
+			delete doc._rev;
+			callbackFunction(true, doc);}
+			
+		});
+}
+
 exports.retrieveByID = retrieveByID;
 exports.retrieveByName = retrieveByName;
+exports.retrieveByName = retrieveByName;
+var n = "rizio";
+
+genQuery("function(doc){if(doc.name.match(\'.*"+n+".*\')!==(null)) {emit(doc.name, doc)};}", function(b, f){console.log(f);}, '','','starwars');
