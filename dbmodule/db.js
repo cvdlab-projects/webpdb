fe206185cd6184bb9fba601083be060211e672d1
Francesco Furiani <francesco@furio.me>
2012-05-18 17:23:24 +0200

    dbmodule: intendation importer: ulimit retrieval for batch processing limitation

--- dbmodule/db.js
+++ dbmodule/db.js
@@ -4,17 +4,15 @@ var JSONDB = require('./databases.json');
 
 //Reads the .json File with configuration informations, such as db host, port, username and password.
 var setup = exports.setup = function (options, callback) {
-
-// Gets info from the config file and set connection configuration. 
-var host = parsedJSON.database.host;
-var port = parsedJSON.database.port;
-var userName = parsedJSON.database.userName;
-var password = parsedJSON.database.password;
+	// Gets info from the config file and set connection configuration. 
+	var host = parsedJSON.database.host;
+	var port = parsedJSON.database.port;
+	var userName = parsedJSON.database.userName;
+	var password = parsedJSON.database.password;
 
 	//Useful if you don't want to change this js file.
-
-		options.host = host;
-		options.port = port;
+	options.host = host;
+	options.port = port;
 	
 	cradle.setup({
 		host: options.host,
@@ -24,11 +22,11 @@ var password = parsedJSON.database.password;
   	var c = new(cradle.Connection)(host, port, {
 		auth: {username: userName, password: password}
 	});
-  var db =  c.database(getDB(options.keyDB));
-  return db; // Returns the istance of the
+	
+	var db =  c.database(getDB(options.keyDB));
+	return db; // Returns the istance of the
 };
 
 var getDB = function(key){
-return JSONDB[key];
-}
-
+	return JSONDB[key];
+};
--- importer/testModule.js
+++ importer/testModule.js
@@ -1,4 +1,4 @@
 var importer = require("./import")
 
-// (rootDir, isRecursive, username, password, dbname, hostname, port)
-importer.runImport("../testdata/lg", false, "webpdb", "w3bpdb", "pdbimporttest", "127.0.0.1", 5984);
+// (rootDir, isRecursive, dbType)
+importer.runImport("../testdata/lg", false, "proteins");
