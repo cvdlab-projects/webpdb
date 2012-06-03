4662d6aa53822a29a5f22609a9b9ef287d9cf406
Dario Pasquantonio <dario@Dario-Mac.local>
2012-06-03 17:42:21 +0200

    hash

--- dbmodule/hash.js
+++ dbmodule/hash.js
@@ -3,10 +3,11 @@ var crypto = require('crypto');
 var createHash = function(input){
 var s = "";
 for(el in input){
-	s+= input[el].toString; 
+	s+= input[el].toString(); 
 }	
 var hash = crypto.createHash('md5');
 hash.update(s);
+console.log(s)
 return hash.digest('hex');}
 
 exports.createHash = createHash;
