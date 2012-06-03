2636f4f7bb6f2844a2507b6c027ae5943e20704d
Francesco Furiani <francesco@furio.me>
2012-06-03 17:31:11 +0200

    dbmodule: hash bug generation

--- dbmodule/hash.js
+++ dbmodule/hash.js
@@ -1,12 +1,13 @@
 var crypto = require('crypto');
 
 var createHash = function(input){
-var s = "";
-for(el in input){
-	s+= input[el].toString; 
-}	
-var hash = crypto.createHash('md5');
-hash.update(s);
-return hash.digest('hex');}
+  var s = "";
+  for(el in input){
+	  s += input[el].toString(); 
+  }	
+  var hash = crypto.createHash('md5');
+  hash.update(s);
+  return hash.digest('hex');
+};
 
 exports.createHash = createHash;
